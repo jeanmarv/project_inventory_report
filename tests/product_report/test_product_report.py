ff751114ed9b34cf711e5ad35eb2d862104c7f67
Jean Marcel <jeanvanzella91@gmail.com>
2022-08-25 22:28:03 -0300

    req 2, test product report

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,25 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    data = Product(
+        1,
+        'Leite',
+        'LongaVida',
+        '2022-01-01',
+        '2022-06-01',
+        '123789',
+        'Ambiente refrigerado'
+    )
+
+    data_repr = data.__repr__()
+
+    expected = (
+        "O produto Leite"
+        " fabricado em 2022-01-01"
+        " por LongaVida com validade"
+        " até 2022-06-01"
+        " precisa ser armazenado Ambiente refrigerado."
+    )
+
+    assert data_repr == expected
