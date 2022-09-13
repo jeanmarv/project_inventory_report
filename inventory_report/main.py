9d0974cc06310a95b97cda4e9e0cad7cb72ca9dd
Jean Marcel <jeanvanzella91@gmail.com>
2022-09-13 12:41:36 -0300

    req 11, generating report by command

--- inventory_report/main.py
+++ inventory_report/main.py
@@ -1,2 +1,9 @@
+import sys
+from inventory_report.inventory.inventory import Inventory
+
+
 def main():
-    pass
+    if len(sys.argv) != 3:
+        return sys.stderr.write('Verifique os argumentos\n')
+    data = Inventory.import_data(sys.argv[1], sys.argv[2])
+    return sys.stdout.write(data)
